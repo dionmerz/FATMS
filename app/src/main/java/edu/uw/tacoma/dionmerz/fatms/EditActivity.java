98104c5942282d6b333b96887a05033e06eb833f
dionmerz <dionmerz@gmail.com>
2016-12-05 20:20:50 -0800

    Changed button styles

--- app/src/main/java/edu/uw/tacoma/dionmerz/fatms/EditActivity.java
+++ app/src/main/java/edu/uw/tacoma/dionmerz/fatms/EditActivity.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
